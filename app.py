59dc83b44f42cfe6c44b25107aeb46ecb436ee3b
Dedsys <yahavfeldman4@gmail.com>
2025-12-16 17:32:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+    print("Database tables created successfully!")
 login_manager = LoginManager()
 login_manager.init_app(app)
 
