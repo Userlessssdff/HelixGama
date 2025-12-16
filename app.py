41a8ec638ca95fabc0739928ff0dd6c7910c56fd
Dedsys <yahavfeldman4@gmail.com>
2025-12-16 17:00:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1300,8 +1300,10 @@ def contact_form():
     send_email(admin_email, subject, body)
     return jsonify({'success': True})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
